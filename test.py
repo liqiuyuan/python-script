e360a165c0b75a82aaa8c09a0b409d0acec91448
liqiuyuan <liqiuyuanforever@163.com>
2018-06-08 10:53:52 +0800

    add test.py

---
+++ test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#-*- encoding: utf-8 -*-
+#!/usr/bin/env python
+#-*- encoding: utf-8 -*-
+
+from functools import wraps
+from datetime import datetime
+
+def log_func(func):
+    @wraps(func)
+    def logger(*args, **kwargs):
+        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print('[%s] Start %s(%s, %s)...' % (now, func.__name__, args, kwargs))
+        return func(*args, **kwargs)
+    return logger
+
+@log_func
+def addition_func(x):
+   """Do some math."""
+   return x + x
+
+
+result = addition_func(4)
+print result
+# Output: addition_func was called
