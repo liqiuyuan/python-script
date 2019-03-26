d7efef80872e9ea98b8b0329b33881f44da47397
liqiuyuan <liqiuyuanforever@163.com>
2019-03-26 15:08:29 +0800

    python3 实现远程linux

---
+++ ssh.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import paramiko
+import sys
+import argparse
+import socket
+import getpass
+
+"""
+python3 实现远程登录linux，类似ssh效果
+python3 ssh.py host_ip [username]
+"""
+
+try:
+    import termios
+    import tty
+    has_termios = True
+except ImportError:
+    has_termios = False
+
+
+def interactive_shell(chan):
+    if has_termios:
+        posix_shell(chan)
+    else:
+        windows_shell(chan)
+
+
+def posix_shell(chan):
+    import select
+    oldtty = termios.tcgetattr(sys.stdin)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        tty.setcbreak(sys.stdin.fileno())
+        chan.settimeout(0.0)
+        while True:
+            r, w, e = select.select([chan, sys.stdin], [], [])
+            if chan in r:
+                try:
+                    x = chan.recv(1024)
+                    if len(x) == 0:
+                        print('\r\n*** EOF ***\r\n',)
+                        break
+                    sys.stdout.write(x.decode('utf8'))
+                    sys.stdout.flush()
+                except socket.timeout:
+                    pass
+            if sys.stdin in r:
+                x = sys.stdin.read(1)
+                if len(x) == 0:
+                    break
+                chan.send(x.encode('utf8'))
+    finally:
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, oldtty)
+
+
+def windows_shell(chan):
+    import threading
+    sys.stdout.write("Line-buffered terminal emulation. Press F6 or ^Z to send EOF.\r\n\r\n")
+
+    def writeall(sock):
+        while True:
+            data = sock.recv(256)
+            if not data:
+                sys.stdout.write('\r\n*** EOF ***\r\n')
+                sys.stdout.flush()
+                break
+            sys.stdout.write(data.decode('utf8'))
+            sys.stdout.flush()
+    writer = threading.Thread(target=writeall, args=(chan,))
+    writer.start()
+    try:
+        while True:
+            d = sys.stdin.read(1)
+            if not d:
+                break
+            chan.send(d.encode('utf8'))
+    except EOFError:
+        # user hit ^Z or F6
+        pass
+
+
+class AllowAnythingPolicy(paramiko.MissingHostKeyPolicy):
+    def missing_host_key(self, client, hostname, key):
+        return
+
+
+def main(hostname, username):
+    paramiko.util.log_to_file('/tmp/paramiko.log')
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(AllowAnythingPolicy())
+    client.connect(hostname, username=username)
+    channel = client.invoke_shell()
+    interactive_shell(channel)
+    channel.close()
+    client.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Connect over SSH')
+    parser.add_argument('hostname', help='Remote machine name')
+    parser.add_argument('username', default=getpass.getuser(), nargs='?', help='Username on the remote machine')
+    args = parser.parse_args()
+    main(args.hostname, args.username)
